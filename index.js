5a0d16327c9d81faad212fbe3bed0248d4421f31
eugenunez <eugeiq@gmail.com>
2021-02-11 17:58:54 -0300

    traje los primeros 20 comics

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-fetch("http://gateway.marvel.com/v1/public/comics?ts=1&apikey=4d140645edcdb0c22d45f34f5fd8098a")
-.then((data)=>{
-return data.json()
-})
-.then((info)=>{
-    console.log(info)
+let comics=[]
 
-const portadaComic=document.querySelector("article");
+fetch("http://gateway.marvel.com/v1/public/comics?apikey=4d140645edcdb0c22d45f34f5fd8098a")
+.then((res)=>{
+return res.json()
+})
+.then((data)=>{
+    console.log(data)
+    const portadaComic=document.querySelector("article");
+    comics = data.data.results
+
+    console.log(comics)
+
+    comics.map((comic)=>{
+        portadaComic.innerHTML+=`
+        <div class="portada-comic">
+        <img src="${comic.thumbnail.path}.jpg"></img>
+        <div>
+        <div class="info-portada">${comic.title}<div>
+        </div>
+        
+        `
+
+    })
+    console.log(seccion)
+
+/* const portadaComic=document.querySelector("article");
 
 portadaComic.innerHTML=`
 <div class="portada-comic">
-<img src="img/wonder-woman.jpg"></img>
+<img src="${data.thumbnail}.jpg"></img>
 <div>
-<div class="info-portada">Hola soy una portada de ejemplo<div>
+<div class="info-portada">${data.title}<div>
 </div>
 
-` 
-});
+`  */
+})
+
