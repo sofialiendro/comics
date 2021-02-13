221a19e809c6a5eb72e7e3385ba7a82f28256b37
Sofia Liendro <sofialiendro.s@gmail.com>
2021-02-13 16:17:34 -0300

    cambio de pagina

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
+// let comics=[]
+
+// fetch("http://gateway.marvel.com/v1/public/comics?apikey=4d140645edcdb0c22d45f34f5fd8098a")
+// .then(res=>res.json())
+// .then((data)=>{
+//     console.log(data)
+//     const portadasComic=document.getElementById("comics");
+//     comics = data.data.results
+
+//     comics.map((comic)=>{
+//         portadasComic.innerHTML+=`
+//         <article>
+//         <div class="portada-comic">
+//         <img src="${comic.thumbnail.path}.jpg"></img>
+//         <div>
+//         <div class="info-portada">${comic.title}<div>
+//         </div>
+//         </article>
+        
+//         `
+//     })
+   
+// })
+
+
+
+
 let comics=[]
 
-fetch("http://gateway.marvel.com/v1/public/comics?apikey=4d140645edcdb0c22d45f34f5fd8098a")
+const buscarInfo = (url) => {
+fetch("https://gateway.marvel.com/v1/public/comics?apikey=4d140645edcdb0c22d45f34f5fd8098a&limit=15&orderBy=title")
 .then(res=>res.json())
 .then((data)=>{
     console.log(data)
     const portadasComic=document.getElementById("comics");
-    comics = data.data.results
+    comics = data.data.results;
+
+    const link = document.querySelector(".proxima-pagina");
+    
+      link.onclick = (e) => {
+        e.preventDefault()
+        buscarInfo(comics.title)
+      }
 
     comics.map((comic)=>{
         portadasComic.innerHTML+=`
@@ -19,6 +54,11 @@ fetch("http://gateway.marvel.com/v1/public/comics?apikey=4d140645edcdb0c22d45f34
         
         `
     })
-   
+    
 })
+}
+
+buscarInfo("https://gateway.marvel.com/v1/public/comics")
+
+
 
